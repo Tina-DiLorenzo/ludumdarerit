d606c815a594069a00006d25894592502cad47c7
Tina-DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2021-04-26 17:08:17 -0400

    Main Scene talking is complete!

--- Ligma Ball Pit/Assets/Scripts/TalkToPlayer.cs
+++ Ligma Ball Pit/Assets/Scripts/TalkToPlayer.cs
@@ -10,47 +10,77 @@ public class TalkToPlayer : MonoBehaviour
     private AudioSource talk;
     public CanvasGroup talkArea;
     public Text talkText;
-    private bool talking;
+    public static bool talking = false;
     private Queue<char> parser = new Queue<char>();
     private float timer;
+    public static bool canSkip;
+    Queue<string> scripts = new Queue<string>();
+    public Text nameField;
+
+    //Script chat 
+    //Yes i originally said this would be .txt based. Then i looked at calc grade ;_; so we are speed running
+    string script1 = "Pizza Man: Mamamia why does the ball pit smell like blood?";
+    string script2 = "Pizza Man: That's a spicy sauce!";
+    string script4 = "Pizza Man: Would you like a sawcon shake?";
+    string script5 = "Pizza Man: Want some ligma pizza? The pizza is so good that I wanna ligma fingers!";
+    string[] scriptToSay;
+    public static int scriptCount = 0;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        canSkip = false;
         timer = 0;
         talkArea.alpha = 0;
         talk = this.gameObject.GetComponent<AudioSource>();
         talkText.text = "";
+        scripts.Enqueue(script2);
+        scripts.Enqueue(script5);
+        scripts.Enqueue(script1);
+        scripts.Enqueue(script4);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(talking)
+        if (talking)
         {
             timer += Time.deltaTime;
-            if(timer > .2)
+            if(timer > .02)
             {
                 timer = 0;
                 if(parser.Count != 0) talkText.text = talkText.text + parser.Dequeue();
-                else
-                {
-                    talking = false;
-                    talkArea.alpha = 0;
-
-                }
+                canSkip = true;
             }
+        }
+        else if(scriptCount <= 0)
+        {
+            talking = false;
+            talkArea.alpha = 0;
+            talk.Stop();
+            PlayerMove.running = true;
+            canSkip = false;
         }
     }
 
     private void OnMouseDown()
     {
-        if(Vector2.Distance(this.gameObject.transform.position, player.gameObject.transform.position) < radius)
+        if(Vector2.Distance(this.gameObject.transform.position, player.gameObject.transform.position) < radius && !talking 
+                            && scripts.Count > 0)
         {
             talking = true;
             PlayerMove.running = false;
+            talkText.text = "";
             talkArea.alpha = 1;
-            Parse("mama mia!");
+            parser.Clear();
+            GetCurrentScript();
+            Parse(scriptToSay[1]);
+            nameField.text = scriptToSay[0];
+
+            canSkip = false;
             talk.Play();
         }
     }
@@ -62,6 +92,8 @@ public class TalkToPlayer : MonoBehaviour
             parser.Enqueue(str[i]);
         }
     }
-
-
+    private void GetCurrentScript()
+    {
+        scriptToSay = scripts.Dequeue().Split(':');
+    }
 }
