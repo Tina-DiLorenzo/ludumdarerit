f6745ebf519baaf89102155725e539273ae353a1
Van Phan <80922094+Van-Phan-001@users.noreply.github.com>
2021-07-19 15:14:15 -0400

    Created a item scriptable object
    scriptable object thats used to hold all the item information (points, sprite, size of collider, etc)

---
+++ Ligma Ball Pit/Assets/ScriptableObjects/ItemInfo.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInfo : ScriptableObject
+{
+    #region fields
+    public string itemName;
+    public float points;
+    public Sprite itemImage;
+    public float colliderSizeX;
+    public float colliderSizeY;
+    #endregion 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    #region link
+    //https://www.youtube.com/watch?v=aPXvoWVabPY
+    #endregion
+}
